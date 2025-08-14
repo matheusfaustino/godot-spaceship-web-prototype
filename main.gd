4c683f9275b0b996a0fdeda5bb630085f2f1b9a8
Matheus Faustino <matheuspfaustino@gmail.com>
2025-08-15 00:24:12 +0200

    Fixed Screen scale issue on mobile browser by chaning scale mode from integer to fractional

--- main.gd
+++ main.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-@onready var window : Window = get_window()
-@onready var viewport : Viewport = get_viewport()
-
 func _ready() -> void:
-	var real_window_size = window.size
-	var game_window_size = viewport.get_visible_rect().size
-	var scale = min(real_window_size.x / game_window_size.x, real_window_size.y / game_window_size.y)
-	window.content_scale_size = Vector2i(game_window_size.x * scale, game_window_size.y * scale)
+	# fixed screen issue with fractional scale instead of this hack
+	
+	#var real_size = ScreenScale.calculate_real_size()
+	#ScreenScale.apply_scale_to_window(real_size)
+	
+	#if real_size:
+		#$Control.size = real_size
+	pass
